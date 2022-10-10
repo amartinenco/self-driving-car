7a310d8ac15d68b0735ae58b691184102c657214
Andrei M <andreimartinenco.dev@gmail.com>
2022-10-10 18:59:58 -0400

    neural network

--- car.js
+++ car.js
@@ -1,6 +1,6 @@
 class Car {
   
-  constructor(x, y, width, height, controlType, maxSpeed = 3) {
+  constructor(x, y, width, height, controlType, maxSpeed = 7) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -14,8 +14,13 @@ class Car {
     this.angle = 0;
     this.damaged = false;
 
+    this.useBrain = controlType == "AI";
+
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork(
+        [this.sensor.rayCount, 6, 4]
+      );
     }
     this.controls = new Controls(controlType);
   }
@@ -27,7 +32,18 @@ class Car {
       this.damaged=this.#assessDamage(roadBorders, traffic);
     }
     if (this.sensor) {
-      this.sensor.update(roadBorders, traffic);
+      this.sensor.update(roadBorders,traffic);
+      const offsets=this.sensor.readings.map(
+          s=>s==null?0:1-s.offset
+      );
+      const outputs=NeuralNetwork.feedForward(offsets, this.brain);
+      // console.log(outputs);
+      if(this.useBrain){
+          this.controls.forward = outputs[0];
+          this.controls.left = outputs[1];
+          this.controls.right = outputs[2];
+          this.controls.reverse = outputs[3];
+      }
     }
   }
 
@@ -108,7 +124,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor=false) {
     if (this.damaged) {
       ctx.fillStyle = "gray";
     } else {
@@ -120,7 +136,7 @@ class Car {
       ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
     ctx.fill();
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
